ec16b119b96378c5cf9c40038fa1b15cf34cdd33
Pedro Henrique Mariano <pedro_mariano.silva@outlook.com>
2023-01-15 18:06:17 -0300

    estrutura basica do projeto teste

---
+++ src/main/java/com/pedro/henrique/attornatus/teste/app/TesteAttornatusApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.henrique.attornatus.teste.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAttornatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAttornatusApplication.class, args);
+	}
+
+}
