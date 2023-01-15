d1e04e34855d6ed01eec2b687d28caa0f644add6
Pedro Henrique Mariano <pedro_mariano.silva@outlook.com>
2023-01-15 19:38:10 -0300

    criado estruturas de entidade pessoa/endereco

---
+++ src/main/java/com/pedro/henrique/attornatus/teste/app/model/entity/Pessoa.java
@@ -0,0 +1,45 @@
+package com.pedro.henrique.attornatus.teste.app.model.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "pessoa", schema= "teste-attornatus")
+@Data
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class Pessoa {	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	@Column(name="data_nasc")
+	private LocalDate dataNasc;
+	
+
+	@OneToMany(cascade= CascadeType.ALL, fetch= FetchType.LAZY, mappedBy= "pessoa")
+	private List<Endereco> enderecos;
+	
+}
+
+
